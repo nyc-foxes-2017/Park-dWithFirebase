03488f80de30699a9743b774a7f64ae952fd7fb5
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-08 13:51:13 -0400

    start implementing login

---
+++ Park-dFirebasePods/AuthProvider.swift
@@ -0,0 +1,64 @@
+//
+//  AuthProvider.swift
+//  Park-dFirebasePods
+//
+//  Created by Sophia Pignatelli on 4/8/17.
+//  Copyright © 2017 Sophia Pignatelli. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+typealias  LoginHandler = (_ msg: String?) -> Void;
+
+struct LoginErrorCode {
+    static let INVALID_EMAIL = "Invalid email, please provide a real email address";
+    static let WRONG_PASSWORD = "Wrong Password, Please Try Again";
+    static let PROBLEM_CONNECTING = "Problem Connecting to Database. Please Try Later";
+    static let USER_NOT_FOUND = "User Not Found, Please Register";
+    static let EMAIL_ALREADY_IN_USE = "Email Already In Use, Please Use Different Email";
+    static let WEAK_PASSWORD = "Password Should Be At Least 6 Characters";
+}
+
+class AuthProvider {
+    private static let _instance = AuthProvider();
+    
+    static var Instance: AuthProvider {
+        return _instance;
+    }
+    
+    func login(withEmail: String, password: String, loginHandler: LoginHandler?) {
+        FIRAuth.auth()?.signIn(withEmail: withEmail, password: password, completion: { (user, error) in
+            if error != nil {
+                self.handleErrors(err: error as! NSError, loginHandler: loginHandler);
+            } else {
+                loginHandler?(nil);
+            }
+        })
+    } //login func
+    
+    private func handleErrors(err: NSError, loginHandler: LoginHandler?) {
+        if let errCode = FIRAuthErrorCode(rawValue: err.code) {
+            switch errCode {
+            case .errorCodeWrongPassword:
+                loginHandler?(LoginErrorCode.WRONG_PASSWORD);
+                break;
+            case .errorCodeInvalidEmail:
+                loginHandler?(LoginErrorCode.INVALID_EMAIL);
+                break;
+            case .errorCodeUserNotFound:
+                loginHandler?(LoginErrorCode.USER_NOT_FOUND);
+                break;
+            case .errorCodeEmailAlreadyInUse:
+                loginHandler?(LoginErrorCode.EMAIL_ALREADY_IN_USE);
+                break;
+            case .errorCodeWeakPassword:
+                loginHandler?(LoginErrorCode.WEAK_PASSWORD);
+                break;
+            default:
+                loginHandler?(LoginErrorCode.PROBLEM_CONNECTING);
+                break;
+            }
+        }
+    }
+} //class
--- Park-dFirebasePods/SignInVC.swift
+++ Park-dFirebasePods/SignInVC.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInVC: UIViewController {
+    private let DRIVER_SEGUE = "DriverVC";
 
     @IBOutlet weak var emailTextField: UITextField!
     
@@ -17,16 +19,32 @@ class SignInVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
 
     @IBAction func login(_ sender: Any) {
-    }
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            AuthProvider.Instance.login(withEmail: emailTextField.text!, password: passwordTextField.text!, loginHandler: { (message) in
+                if message != nil {
+                    self.alertTheUser(title: "Problem With Authentication", message: message!);
+                } else {
+                    print("Login Successfull")
+                }
+            });
+                }
+           }
     
     @IBAction func register(_ sender: Any) {
     }
    
+    private func alertTheUser(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert);
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil);
+        alert.addAction(ok);
+        present(alert, animated: true, completion: nil)
+    }
 
 
 } //class
