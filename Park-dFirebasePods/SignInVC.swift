df03b330f5f3d2b306077d4854ced79da665933d
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-08 11:16:54 -0400

    sign in form displays

---
+++ Park-dFirebasePods/SignInVC.swift
@@ -0,0 +1,34 @@
+//
+//  SignInVC.swift
+//  Park-dFirebasePods
+//
+//  Created by Sophia Pignatelli on 4/8/17.
+//  Copyright © 2017 Sophia Pignatelli. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    @IBAction func register(_ sender: Any) {
+    }
+   
+
+
+} //class
+
+
