2194ca5e61e83ed5991d44138b2dcb2577cfbd4b
Sophia Pignatelli <pignatelli@fordham.edu>
2017-04-08 10:35:32 -0400

    Initial Commit

---
+++ Park-dFirebasePods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Park-dFirebasePods
+//
+//  Created by Sophia Pignatelli on 4/8/17.
+//  Copyright © 2017 Sophia Pignatelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
